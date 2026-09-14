a2dc5720355fab9eaf242ec1bc0a3f0aa1ab1783
AbhishekxMaurya <97535930+AbhishekxMaurya@users.noreply.github.com>
2026-09-14 21:18:39 +0530

    Revert "feat: add category management pages"

--- admin-portal/app/bootstrap/page.tsx
+++
@@ -1,7 +0,0 @@
-import BootstrapForm from "@/components/BootstrapForm";
-
-export const metadata = { title: "Set up operations | ZARI" };
-
-export default function BootstrapPage() {
-  return <BootstrapForm />;
-}
